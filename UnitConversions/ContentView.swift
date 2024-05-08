63744068e580b22c16ed194197e5aff7d4a3b786
leotn03 <leo.tn03@icloud.com>
2024-05-07 21:38:42 -0500

    Created app for temperatures conversions

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -8,14 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var firstTemperature = "Celsius"
+    @State private var secondTemperature = "Fahrenheit"
+    @State private var temperature = 0.0
+    @FocusState private var temperatureIsFocused: Bool
+    
+    let temperatures = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var result: Double {
+        let unit: UnitTemperature
+        
+        switch firstTemperature {
+            case "Celsius":
+                unit = UnitTemperature.celsius
+            case "Fahrenheit":
+                unit = UnitTemperature.fahrenheit
+            default:
+                unit = UnitTemperature.kelvin
         }
-        .padding()
+        
+        let t = Measurement(value: temperature, unit: unit)
+        
+        switch secondTemperature {
+            case "Celsius":
+                return t.converted(to: .celsius).value
+            case "Fahrenheit":
+                return t.converted(to: .fahrenheit).value
+            default:
+                return t.converted(to: .kelvin).value
+            
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Temperatures", selection: $firstTemperature) {
+                        ForEach(temperatures, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("From: ")
+                }
+                
+                Section {
+                    Picker("Temperatures", selection: $secondTemperature) {
+                        ForEach(temperatures, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("To: ")
+                }
+                
+                Section {
+                    TextField("Temperature to convert", value: $temperature, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($temperatureIsFocused)
+                    
+                } header: {
+                    Text("Enter the temperature that you want to convert")
+                }
+                
+                Section {
+                    Text("\(result.formatted())")
+                } header: {
+                    Text("Result:")
+                }
+            }
+            .navigationTitle("Unit Conversions")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        temperatureIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
